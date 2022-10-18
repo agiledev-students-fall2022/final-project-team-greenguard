562ae6fa47b0128f47db598f2fd975eb03778806
vicka1228 <bm3001@nyu.edu>
2022-10-18 18:04:39 -0400

    added base message component

--- front-end/src/pages/Messages.js
+++ front-end/src/pages/Messages.js
@@ -7,7 +7,12 @@ function Messages() {
     <div className="Messages">
         <h1> Message Page </h1>
         <SearchBar></SearchBar>
-        
+        <MessageCard
+          img="/resowLogo.png"
+          name="user x"
+          date="mm.dd.yyyy"
+          message="when are you coming to pick the bunnie rabbits"
+        />
         <Link to = "/User#?ChatInbox"> ChatInbox </Link>
     </div>
   );
