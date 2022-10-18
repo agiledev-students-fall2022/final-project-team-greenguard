1dd46765d1d80f03a36484c4db1f9afec6e1112c
vicka1228 <bm3001@nyu.edu>
2022-10-18 18:03:56 -0400

    added search bar component

--- front-end/src/pages/Messages.js
+++ front-end/src/pages/Messages.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
-
+import SearchBar from '../components/SearchBar';
+import MessageCard from '../components/Message'
 
 function Messages() {
   return (
     <div className="Messages">
         <h1> Message Page </h1>
+        <SearchBar></SearchBar>
+        
         <Link to = "/User#?ChatInbox"> ChatInbox </Link>
     </div>
   );
