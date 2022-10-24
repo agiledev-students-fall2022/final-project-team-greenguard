c53d82a69cb27a25962b985aff113d9fd16cdcf8
vicka1228 <bm3001@nyu.edu>
2022-10-23 20:02:36 -0400

    undoing mistake

--- front-end/src/pages/Messages.js
+++ front-end/src/pages/Messages.js
@@ -1,8 +1,4 @@
 import { Link } from "react-router-dom";
-import SearchBar from '../components/SearchBar';
-import MessageCard from '../components/Message'
-import axios from "axios"
-import { useEffect,useState } from 'react';
 
 function Messages() {
   return (
