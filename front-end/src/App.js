05d7ac3b326922e202881bcb7e25bdb1ab89af37
Sachin Iyer <sachinjiyer@gmail.com>
2022-10-24 02:51:59 -0400

    make sure user profile is being routed too

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -36,26 +36,29 @@ function App() {
               descriptions="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " />} />
 
 
-              {/* a route for the SignIn page */}
-              <Route path = "/SignIn" element={<SignIn
-                                                profileURL = "/resowLogo.png"/>} />
-              
-              {/* a route for the Register page */}
-              <Route path = "/Register" element={<Register/>} />
+            {/* a route for the SignIn page */}
+            <Route path="/SignIn" element={<SignIn
+              profileURL="/resowLogo.png" />} />
 
-              {/* a route for the items list page */}
-              <Route path = "/Map/ItemsList" element={<ItemsList/>} />
+            {/* a route for the Register page */}
+            <Route path="/Register" element={<Register />} />
+
+            {/* a route for the items list page */}
+            <Route path="/Map/ItemsList" element={<ItemsList />} />
+
+            {/* a route for the edit profile page */}
+            <Route path="/UserProfile" element={<UserProfile />} />
 
 
 
-              {/* a route for the edit profile page */}
-              <Route path = "/UserProfile/EditProfile" element={<EditProfile 
-                                                                  source = "https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg"
-                                                                  fullname = "Foo Bar" 
-                                                                  email = "foobar@school.edu" 
-                                                                  username = "foobar1" 
-                                                                  tel = "+9711234567" 
-                                                                  location = "New York, USA"/>} />
+            {/* a route for the edit profile page */}
+            <Route path="/UserProfile/EditProfile" element={<EditProfile
+              source="https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg"
+              fullname="Foo Bar"
+              email="foobar@school.edu"
+              username="foobar1"
+              tel="+9711234567"
+              location="New York, USA" />} />
 
             {/* a route for the items list page */}
             <Route path="/ItemDetails" element={<ItemDetails
@@ -67,8 +70,6 @@ function App() {
               descriptions="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " />}
             />
 
-            {/* a route for the edit profile page */}
-            <Route path="/UserProfile/EditProfile" element={<EditProfile />} />
 
             {/* a route for the saved post page */}
             <Route path="/UserProfile/SavedPost" element={<SavedPost />} />
