c1c3330155a604f6f46dc932ca9e82f7921cfbee
vicka1228 <bm3001@nyu.edu>
2022-10-23 20:00:40 -0400

    added base code

--- front-end/src/pages/Messages.js
+++ front-end/src/pages/Messages.js
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
-
+import SearchBar from '../components/SearchBar';
+import MessageCard from '../components/Message'
+import axios from "axios"
+import { useEffect,useState } from 'react';
 
 function Messages() {
   return (
